2d4e1e78c85b9fb334aadcb2d8746cafaf233671
Joy <mmehedihasanjoyv@gmail.com>
2022-10-19 15:53:46 +0600

    added login and sign up link in header

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBywBQXJaJH0mDIV0WDxB-Xxl2zSmP9uqs",
+  authDomain: "ema-john-simple-1b85a.firebaseapp.com",
+  projectId: "ema-john-simple-1b85a",
+  storageBucket: "ema-john-simple-1b85a.appspot.com",
+  messagingSenderId: "469488399585",
+  appId: "1:469488399585:web:a9a3432242ee67e2da73a0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
